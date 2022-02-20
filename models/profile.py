ca931558e87dec197cf143ab97033c3a07c188b0
Jared Dantis <jareddantis@gmail.com>
2022-02-20 18:15:50 +0800

    models/profile: Add fields and methods for pw hashing

--- models/profile.py
+++ models/profile.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from flask_login import UserMixin
-from typing import List
+from hashlib import pbkdf2_hmac
 from models.credentials import GoogleCredentials, MoodleCredentials
+from os import urandom
+from typing import List, Tuple
 
 @dataclass
 class Profile(UserMixin):
@@ -11,6 +13,12 @@ class Profile(UserMixin):
     # User e-mail address
     email: str
 
+    # User password, salted and hashed
+    hashed_password: str
+
+    # User password salt
+    salt: str
+
     # User profile picture URL
     picture: str
 
@@ -19,3 +27,32 @@ class Profile(UserMixin):
 
     # Linked Moodle account
     moodle_account: MoodleCredentials
+
+
+def hash_password(password: str) -> Tuple[str, str]:
+    """
+    Takes in a password, generates a random salt, and hashes the password
+    with the salt.
+
+    :param password: Password to be hashed
+    :return: Tuple of hashed password and salt, both byte arrays encoded as hex strings
+    """
+
+    salt = urandom(32)
+    hashed = pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    return hashed.hex(), salt.hex()
+
+
+def check_password(password: str, hashed: str, salt: str) -> bool:
+    """
+    Checks if a given password matches a given salted and hashed password.
+
+    :param password: Password to be checked
+    :param hashed: Hashed password (byte array encoded as hex string)
+    :param salt: Salt (byte array encoded as hex string)
+    :return: True if password matches, False otherwise
+    """
+
+    salt_bytes = bytes.fromhex(salt)
+    password_hashed = pbkdf2_hmac('sha256', password.encode('utf-8'), salt_bytes, 100000)
+    return password_hashed.hex() == hashed
