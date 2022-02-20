498570d58ddaff669c4304f5c13667a88adbd1ce
Jared Dantis <jareddantis@gmail.com>
2022-02-20 17:23:34 +0800

    /login: Use custom div for centering

--- views/login.py
+++ views/login.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 login = Blueprint('login', __name__, template_folder='templates')
 
-@login.route('/login', methods=['GET'])
+@login.route('/login', methods=['GET', 'POST'])
 def login_page():
-    return render_template('login.html', full_width='true')
+    if request.method == 'GET':
+        return render_template('login.html', full_width='true')
