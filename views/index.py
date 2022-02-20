bd8bda83388d76198e9995dfc3c5d34891ac7a04
Jared Dantis <jareddantis@gmail.com>
2022-02-20 17:13:49 +0800

    templates: Separate base for logged in state

--- views/index.py
+++ views/index.py
@@ -4,4 +4,4 @@ index = Blueprint('index', __name__, template_folder='templates')
 
 @index.route('/', methods=['GET'])
 def index_page():
-    return render_template('index.html')
+    return render_template('index.html', full_width='true')
