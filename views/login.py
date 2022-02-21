99e998b347d61947d91fcd9ff720f7eae756c519
Jared Dantis <jareddantis@gmail.com>
2022-02-21 13:20:54 +0800

    views/login: Create separate handler for POST

--- views/login.py
+++ views/login.py
@@ -2,7 +2,10 @@ from flask import Blueprint, render_template, request
 
 login = Blueprint('login', __name__, template_folder='templates')
 
-@login.route('/login', methods=['GET', 'POST'])
+@login.route('/login', methods=['GET'])
 def login_page():
-    if request.method == 'GET':
-        return render_template('login.html', full_width='true')
+    return render_template('login.html', full_width='true')
+
+@login.route('/login', methods=['POST'])
+def login_form():
+    pass
