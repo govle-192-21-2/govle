29526bd85aff1cfa5ddc2072ecc14286b5b84898
Jared Dantis <jareddantis@gmail.com>
2022-03-04 02:00:29 +0800

    templates/index: Rename back to homepage

--- views/index.py
+++ views/index.py
@@ -8,4 +8,4 @@ def index_page():
     # Redirect to dashboard if already logged in
     if current_user.is_authenticated:
         return redirect(url_for('dashboard.dashboard_page'))
-    return render_template('index.html', full_width='true')
+    return render_template('homepage.html', full_width='true')
