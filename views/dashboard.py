2a63d22bc2f5b0534e4da118ffa21d14228056d4
Jared Dantis <jareddantis@gmail.com>
2022-03-08 23:06:40 +0800

    templates/b-l-i: Tweak layout for adaptability

--- views/dashboard.py
+++ views/dashboard.py
@@ -6,4 +6,4 @@ dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 @dashboard.route('/dashboard')
 @login_required
 def dashboard_page():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', active_nav='home')
