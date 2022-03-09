2549b88957bb8120db09ce1082870e6e66d298a1
Jared Dantis <jareddantis@gmail.com>
2022-03-10 03:00:14 +0800

    models/credentials: Conform GoogleCredentials to official API library

--- models/credentials.py
+++ models/credentials.py
@@ -1,5 +1,6 @@
 from abc import ABC
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass
@@ -8,19 +9,31 @@ class LearningEnvCredentials(ABC):
 
 
 @dataclass
-class GoogleCredentials():
-    # User ID
-    user_id: str = ''
-
+class GoogleCredentials(LearningEnvCredentials):
     # OAuth2 access token
     access_token: str = ''
 
     # OAuth2 refresh token
     refresh_token: str = ''
 
+    # OAuth2 token URI
+    token_uri: str = ''
+
+    # OAuth2 client ID
+    client_id: str = ''
+
+    # OAuth2 client secret
+    client_secret: str = ''
+
+    # OAuth2 scopes
+    scopes: List[str] = field(default_factory=list)
+
+    # OpenID token
+    id_token: str = ''
+
 
 @dataclass
-class MoodleCredentials():
+class MoodleCredentials(LearningEnvCredentials):
     # Username
     username: str = ''
 
