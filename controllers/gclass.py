99c849f4f8bc42501be59c380737744df624075c
Jared Dantis <jareddantis@gmail.com>
2022-03-30 13:39:48 +0800

    controllers/gclass: Don't save creds as prop

--- controllers/gclass.py
+++ controllers/gclass.py
@@ -9,7 +9,6 @@ from .learning_env import LearningEnv
 class GoogleClassroomClient(LearningEnv):
     def __init__(self, credentials: Dict, on_token_refresh: Callable[[GoogleCredentials], None]):
         # Create Google Credentials object from the given credentials
-        self._credentials = credentials
         creds = Credentials.from_authorized_user_info(credentials)
 
         # Check if credentials are valid
@@ -18,7 +17,7 @@ class GoogleClassroomClient(LearningEnv):
                 creds.refresh(Request())
 
                 # Update credentials
-                self._credentials = GoogleCredentials(
+                on_token_refresh(GoogleCredentials(
                     email=credentials['email'],
                     token=creds.token,
                     refresh_token=creds.refresh_token,
@@ -29,17 +28,12 @@ class GoogleClassroomClient(LearningEnv):
                     id_token=creds.id_token,
                     expiry=f'{creds.expiry.isoformat()}Z',
                     user_id=credentials['user_id']
-                )
-                on_token_refresh(self._credentials)
+                ))
             else:
                 raise Exception('Invalid credentials')
 
         # Create Google Classroom API service
         self.service = build('classroom', 'v1', credentials=creds)
-    
-    @property
-    def credentials(self) -> GoogleCredentials:
-        return self._credentials
     
     def get_classes(self) -> List[GoogleClass]:
         results = self.service.courses().list(pageSize=10).execute()
