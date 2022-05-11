1e59162734c588da6d03c678df5a0b03f7e4e5ae
Jared Dantis <jareddantis@gmail.com>
2022-05-12 03:41:56 +0800

    models: Create model for deadlines

---
+++ models/deadline.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from json import JSONEncoder
+
+
+@dataclass
+class Deadline:
+    """
+    Dataclass for a deadline.
+    """
+
+    # Name of the assignment
+    name: str
+
+    # Deadline date, in Unix timestamp format
+    timestamp: int
+
+    # Course to which the deadline belongs
+    course: str
+
+    # Platform that the course is on
+    platform: str
+
+    # URL to the assignment, if any
+    url: str = None
+
+
+class DeadlineEnc(JSONEncoder):
+    """
+    JSONEncoder for Deadline objects.
+    """
+
+    def default(self, o):
+        if isinstance(o, Deadline):
+            return {
+                'name': o.name,
+                'timestamp': o.timestamp,
+                'course': o.course,
+                'platform': o.platform,
+                'url': o.url
+            }
+        return JSONEncoder.default(self, o)
